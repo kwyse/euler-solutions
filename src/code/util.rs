5adf61b9954b78fd99eddbfea96b3015fe936086
Krishan Wyse <kwysek@gmail.com>
2016-10-09 20:38:20 +0100

    Remove unnecessary parens and add doc comments

--- src/code/util.rs
+++ src/code/util.rs
@@ -1,14 +1,20 @@
 //! Misc. utility functions
 
+/// Utility functions for strings or string slices
 pub mod string {
+
+    /// Doubles an unsigned integer represented as a string
+
+    /// Iterates from the right side, doubling each individual character in
+    /// turn and adding a carry when necessary. The string will grow by one
+    /// if there is an outstanding carry.
     pub fn double_num(num: &str) -> String {
         let mut carry = false;
-
         let mut doubled_chars = num.chars()
             .rev()
             .map(|c| {
                 let already_carry = carry;
-                let mut x = (c.to_digit(10).unwrap()) * 2;
+                let mut x = c.to_digit(10).unwrap() * 2;
 
                 if x > 9 {
                     x %= 10;
@@ -30,6 +36,7 @@ pub mod string {
             .collect()
     }
 
+    /// Sums the individual digits of an unsigned integer represented as a string
     pub fn sum_digits(num: &str) -> u32 {
         num.chars()
             .map(|c| c.to_digit(10).unwrap())
