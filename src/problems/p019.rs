870f9e8ac0fad8375b785cfa3e1fd692156c36bd
Krishan Wyse <kwysek@gmail.com>
2018-05-30 20:52:51 +0100

    Simplify p019 by removing Weekday enum

--- src/problems/p019.rs
+++ src/problems/p019.rs
@@ -19,47 +19,26 @@ fn test() {
 }
 
 fn problem() -> u16 {
-    let mut day_counter = NUM_DAYS_PER_MONTH_PER_LEAP_CYCLE[0..12].iter().sum();
+    // Skip 1900
+    let mut day_counter: u16 = NUM_DAYS_PER_MONTH_PER_LEAP_CYCLE[0..12].iter().sum();
 
-    let mut sundays_counter = 0;
+    let mut sunday_counter = 0;
     for month in 0..(12 * 100) {
-        if weekday_from_days_since_1900(day_counter) == Weekday::Sunday {
-            sundays_counter += 1;
+        if day_counter % DAYS_IN_WEEK == DAYS_FROM_MONDAY_TO_SUNDAY {
+            sunday_counter += 1;
         }
 
         day_counter += NUM_DAYS_PER_MONTH_PER_LEAP_CYCLE[month % (12 * 4)];
     }
 
-    sundays_counter
+    sunday_counter
 }
 
+const DAYS_IN_WEEK: u16 = 7;
+const DAYS_FROM_MONDAY_TO_SUNDAY: u16 = 6;
 const NUM_DAYS_PER_MONTH_PER_LEAP_CYCLE: [u16; 12 * 4] = [
     31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
     31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
     31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
     31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
 ];
-
-#[derive(PartialEq)]
-enum Weekday {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday,
-}
-
-fn weekday_from_days_since_1900(days: u16) -> Weekday {
-    match days % 7 {
-        0 => Weekday::Monday,
-        1 => Weekday::Tuesday,
-        2 => Weekday::Wednesday,
-        3 => Weekday::Thursday,
-        4 => Weekday::Friday,
-        5 => Weekday::Saturday,
-        6 => Weekday::Sunday,
-        _ => unreachable!(),
-    }
-}
