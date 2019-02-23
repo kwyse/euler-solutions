e32b0c9adec3a7f0b0dcd4995cd19dbc0f41846a
Krishan Wyse <kwysek@gmail.com>
2019-02-23 21:22:59 +0000

    Implement new solve! macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,5 @@
 //! Abstractions over Project Euler problems
 
-#![allow(dead_code)]
-#![feature(iterator_step_by)]
-
-extern crate kb;
-extern crate num;
-
-pub use code::binomial as bi;
-pub use code::collatz as collatz;
-pub use code::fibonacci as fib;
-pub use code::matrix as mat;
-pub use code::prime as prime;
-pub use code::triangle as tri;
-pub use code::util as ut;
-
 #[macro_export]
 macro_rules! solve {
     (expecting_answer: $expected:expr, with: $solution:expr) => {
@@ -25,5 +11,4 @@ macro_rules! solve {
     }
 }
 
-mod code;
 mod problems;
