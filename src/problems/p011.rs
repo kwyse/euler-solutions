aba89d6b38c32b19842e6ec1d4ad5b9362772978
Krishan Wyse <kwysek@gmail.com>
2019-02-23 12:21:44 +0000

    Change function to closure and pass in movement op

--- src/problems/p011.rs
+++ src/problems/p011.rs
@@ -3,17 +3,17 @@
 solve!(expecting_answer: 70_600_674, with: || {
     let grid = include_str!("../../resources/p011")
         .split_whitespace()
-        .filter_map(|s| s.parse::<u32>().ok())
+        .filter_map(|n| n.parse::<u32>().ok())
         .collect::<Vec<_>>();
 
-    fn product(grid: &[u32], indices: impl Iterator<Item = usize>) -> u32 {
-        indices.map(|i| grid[i]).product::<u32>()
-    }
+    let calc_product = |movement_op: &Fn(usize) -> usize| -> u32 {
+        (0..4).map(movement_op).map(|i| grid[i]).product::<u32>()
+    };
 
-    let right = |i| product(&grid, i..i + 4);
-    let down = |i| product(&grid, (0..4).map(|j| i + j * 20));
-    let down_right = |i| product(&grid, (0..4).map(|j| (i + j) + j * 20));
-    let down_left = |i| product(&grid, (0..4).map(|j| (i - j) + j * 20));
+    let right = |i| calc_product(&|j| i + j);
+    let down = |i| calc_product(&|j| i + j * 20);
+    let down_right = |i| calc_product(&|j| (i + j) + j * 20);
+    let down_left = |i| calc_product(&|j| (i - j) + j * 20);
 
     (0..=(20 * 16)).map(|i| match i % 20 {
         0...3 => right(i).max(down(i)).max(down_right(i)),
