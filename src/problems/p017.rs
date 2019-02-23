340660154524d321f643412a457fee011d32706f
Krishan Wyse <kwysek@gmail.com>
2019-02-23 21:18:11 +0000

    Remove old solution from problem 17

--- src/problems/p017.rs
+++ src/problems/p017.rs
@@ -40,23 +40,3 @@ solve!(expecting_answer: 21_124, with: || {
 
     (1..=1000).map(|n| dbg!(words(n)).len()).sum::<usize>() as u128
 });
-
-// #[allow(dead_code)]
-// fn p017() -> u64 {
-//     use ut::string::ToWords;
-
-//     let mut words = "".to_string();
-//     for i in 1..1001 {
-//         words.push_str(&i.to_words().unwrap());
-//     }
-
-//     words.chars().filter(|&c| c != ' ').count() as u64
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn test() {
-//         assert_eq!(21_124, super::p017());
-//     }
-// }
