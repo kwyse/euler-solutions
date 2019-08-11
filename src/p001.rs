539e7903a1d4b9821b87057abfeea93f3884f029
Krishan Wyse <kwysek@gmail.com>
2019-08-11 21:42:49 +0100

    Add explanation of Guass' method of summing

--- src/p001.rs
+++ src/p001.rs
@@ -10,6 +10,29 @@
 /// This makes use of Guass' method of summing numbers up to a given bound. Since
 /// the product of the input set of numbers is included in the sum twice, it must
 /// be subtracted.
+///
+/// Guass' method involves observing that each number in a sequence ending with
+/// an even number can be paired. For example, for _n_ = 6:
+///
+/// ```
+/// 1 + 6 = 7
+/// 2 + 5 = 7
+/// 3 + 4 = 7
+/// ```
+///
+/// There are three pairs (ie. 6 / 2), so the sum is (6 + 1) * (6 / 2), or
+/// (_n_(_n_ + 1)) / 2.
+///
+/// For sequences that end in an odd number, pad the sequence with a zero:
+///
+/// ```
+/// 0 + 5 = 5
+/// 1 + 4 = 5
+/// 2 + 3 = 5
+/// ```
+///
+/// Now the _n_ represents the sum of each pair, and the (_n_ + 1) / 2 represents
+/// the number of pairs.
 pub fn sum_of_multiples(ns: &[u32], limit: u32) -> u32 {
     let sum_divisible_by = |n| {
         let upper_bound = (limit - 1) / n;
